a4d7b5e9cea870ff3d2af3c4cef12366b60751b5
Momchil Atanasov <momchil.dev@gmail.com>
2019-09-03 22:45:59 +0300

    enhance sprec vec3

--- sprec/vec3.go
+++ sprec/vec3.go
@@ -1,5 +1,7 @@
 package sprec
 
+import "fmt"
+
 func NewVec3(x, y, z float32) Vec3 {
 	return Vec3{
 		X: x,
@@ -8,8 +10,113 @@ func NewVec3(x, y, z float32) Vec3 {
 	}
 }
 
+func ZeroVec3() Vec3 {
+	return Vec3{}
+}
+
+func BasisXVec3() Vec3 {
+	return Vec3{
+		X: 1.0,
+		Y: 0.0,
+		Z: 0.0,
+	}
+}
+
+func BasisYVec3() Vec3 {
+	return Vec3{
+		X: 0.0,
+		Y: 1.0,
+		Z: 0.0,
+	}
+}
+
+func BasisZVec3() Vec3 {
+	return Vec3{
+		X: 0.0,
+		Y: 0.0,
+		Z: 1.0,
+	}
+}
+
+func Vec3Sum(a, b Vec3) Vec3 {
+	return Vec3{
+		X: a.X + b.X,
+		Y: a.Y + b.Y,
+		Z: a.Z + b.Z,
+	}
+}
+
+func Vec3Diff(a, b Vec3) Vec3 {
+	return Vec3{
+		X: a.X - b.X,
+		Y: a.Y - b.Y,
+		Z: a.Z - b.Z,
+	}
+}
+
+func Vec3Prod(vector Vec3, value float32) Vec3 {
+	return Vec3{
+		X: vector.X * value,
+		Y: vector.Y * value,
+		Z: vector.Z * value,
+	}
+}
+
+func Vec3Quot(vector Vec3, value float32) Vec3 {
+	return Vec3{
+		X: vector.X / value,
+		Y: vector.Y / value,
+		Z: vector.Z / value,
+	}
+}
+
+func Vec3Dot(a, b Vec3) float32 {
+	return a.X*b.X + a.Y*b.Y + a.Z*b.Z
+}
+
+func Vec3Cross(a, b Vec3) Vec3 {
+	return Vec3{
+		X: a.Y*b.Z - a.Z*b.Y,
+		Y: a.Z*b.X - a.X*b.Z,
+		Z: a.X*b.Y - a.Y*b.X,
+	}
+}
+
+func UnitVec3(vector Vec3) Vec3 {
+	return Vec3Quot(vector, vector.Length())
+}
+
+func ResizedVec3(vector Vec3, newLength float32) Vec3 {
+	ratio := newLength / vector.Length()
+	return Vec3Prod(vector, ratio)
+}
+
+func InverseVec3(vector Vec3) Vec3 {
+	return Vec3{
+		X: -vector.X,
+		Y: -vector.Y,
+		Z: -vector.Z,
+	}
+}
+
 type Vec3 struct {
 	X float32
 	Y float32
 	Z float32
+}
+
+func (v Vec3) IsZero() bool {
+	return Eq(v.X, 0.0) && Eq(v.Y, 0.0) && Eq(v.Z, 0.0)
+}
+
+func (v Vec3) SqrLength() float32 {
+	return Vec3Dot(v, v)
+}
+
+func (v Vec3) Length() float32 {
+	return Sqrt(Vec3Dot(v, v))
+}
+
+func (v Vec3) GoString() string {
+	return fmt.Sprintf("(%f, %f, %f)", v.X, v.Y, v.Z)
 }
--- sprec/vec3_test.go
+++ sprec/vec3_test.go
@@ -9,8 +9,112 @@ import (
 )
 
 var _ = Describe("Vec3", func() {
+	var nullVector Vec3
+	var firstVector Vec3
+	var secondVector Vec3
+
+	BeforeEach(func() {
+		nullVector = Vec3{}
+		firstVector = Vec3{
+			X: 2.0,
+			Y: 3.0,
+			Z: 4.0,
+		}
+		secondVector = Vec3{
+			X: -1.0,
+			Y: 2.0,
+			Z: 5.0,
+		}
+	})
+
 	Specify("NewVec3", func() {
 		vector := NewVec3(9.8, 2.3, 1.5)
 		Expect(vector).To(HaveVec3Coords(9.8, 2.3, 1.5))
+	})
+
+	Specify("ZeroVec3", func() {
+		vector := ZeroVec3()
+		Expect(vector).To(HaveVec3Coords(0.0, 0.0, 0.0))
+	})
+
+	Specify("BasisXVec3", func() {
+		vector := BasisXVec3()
+		Expect(vector).To(HaveVec3Coords(1.0, 0.0, 0.0))
+	})
+
+	Specify("BasisYVec3", func() {
+		vector := BasisYVec3()
+		Expect(vector).To(HaveVec3Coords(0.0, 1.0, 0.0))
+	})
+
+	Specify("BasisZVec3", func() {
+		vector := BasisZVec3()
+		Expect(vector).To(HaveVec3Coords(0.0, 0.0, 1.0))
+	})
+
+	Specify("Vec3Sum", func() {
+		result := Vec3Sum(firstVector, secondVector)
+		Expect(result).To(HaveVec3Coords(1.0, 5.0, 9.0))
+	})
+
+	Specify("Vec3Diff", func() {
+		result := Vec3Diff(firstVector, secondVector)
+		Expect(result).To(HaveVec3Coords(3.0, 1.0, -1.0))
+	})
+
+	Specify("Vec3Prod", func() {
+		result := Vec3Prod(firstVector, 2.0)
+		Expect(result).To(HaveVec3Coords(4.0, 6.0, 8.0))
+	})
+
+	Specify("Vec3Quot", func() {
+		result := Vec3Quot(firstVector, 2.0)
+		Expect(result).To(HaveVec3Coords(1.0, 1.5, 2.0))
+	})
+
+	Specify("Vec3Dot", func() {
+		result := Vec3Dot(firstVector, secondVector)
+		Expect(result).To(EqualFloat32(24.0))
+	})
+
+	Specify("Vec3Cross", func() {
+		result := Vec3Cross(firstVector, secondVector)
+		Expect(result).To(HaveVec3Coords(7.0, -14.0, 7.0))
+	})
+
+	Specify("UnitVec3", func() {
+		result := UnitVec3(firstVector)
+		Expect(result).To(HaveVec3Coords(0.371390676354, 0.557086014531, 0.742781352708))
+	})
+
+	Specify("ResizedVec3", func() {
+		result := ResizedVec3(firstVector, 10.770329614269)
+		Expect(result).To(HaveVec3Coords(4.0, 6.0, 8.0))
+	})
+
+	Specify("InverseVec3", func() {
+		result := InverseVec3(firstVector)
+		Expect(result).To(HaveVec3Coords(-2.0, -3.0, -4.0))
+	})
+
+	Specify("#IsZero", func() {
+		Expect(nullVector.IsZero()).To(BeTrue())
+		Expect(firstVector.IsZero()).To(BeFalse())
+		Expect(NewVec3(Epsilon, Epsilon, Epsilon).IsZero()).To(BeFalse())
+	})
+
+	Specify("#SqrLength", func() {
+		lng := firstVector.SqrLength()
+		Expect(lng).To(EqualFloat32(29))
+	})
+
+	Specify("#Length", func() {
+		lng := firstVector.Length()
+		Expect(lng).To(EqualFloat32(5.385164807134))
+	})
+
+	Specify("#GoString", func() {
+		result := firstVector.GoString()
+		Expect(result).To(Equal("(2.000000, 3.000000, 4.000000)"))
 	})
 })
