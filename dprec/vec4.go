a2a412c8fbd0df00fec6190f112b8715c10efa90
Momchil Atanasov <momchil.dev@gmail.com>
2020-03-26 22:16:08 +0200

    introduce vec4 type

---
+++ dprec/vec4.go
@@ -0,0 +1,8 @@
+package dprec
+
+type Vec4 struct {
+	X float64
+	Y float64
+	Z float64
+	W float64
+}
---
+++ sprec/vec4.go
@@ -0,0 +1,8 @@
+package sprec
+
+type Vec4 struct {
+	X float32
+	Y float32
+	Z float32
+	W float32
+}
