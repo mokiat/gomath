b12a17dea1987ec681b4c51ae1b9b365eb58f178
Momchil Atanasov <momchil.dev@gmail.com>
2020-03-26 22:15:24 +0200

    introduce mat4 type

---
+++ dprec/mat4.go
@@ -0,0 +1,8 @@
+package dprec
+
+type Mat4 struct {
+	M11, M12, M13, M14 float64
+	M21, M22, M23, M24 float64
+	M31, M32, M33, M34 float64
+	M41, M42, M43, M44 float64
+}
---
+++ sprec/mat4.go
@@ -0,0 +1,8 @@
+package sprec
+
+type Mat4 struct {
+	M11, M12, M13, M14 float32
+	M21, M22, M23, M24 float32
+	M31, M32, M33, M34 float32
+	M41, M42, M43, M44 float32
+}
